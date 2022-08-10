a6c3cf1927b854e88bfb2955d652f7c3ef494ad7
gmsales <gmsales@google.com>
2022-08-10 17:47:18 +0000

    header style

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import './App.css'
 import { Summary } from './components/Summary'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const attributes = {
-    name: {size: "12", name: "Titulo"}
+    name: {size: "6", name: "Titulo"},
+    price: {size: "2", name: "Preço"},
+    type: {size: "2", name: "Categoria"},
+    date: {size: "2", name: "Data"}
   };
 
   return (
-    TransactionTable(attributes)
+    <div className="container">{TransactionTable(attributes)}</div>
   )
 }
 
--- src/components/transaction-table-header/index.jsx
+++ src/components/transaction-table-header/index.jsx
@@ -4,7 +4,7 @@ function TransactionTableHeader(attributes){
     for(let i = 0; i < keys.length; i++){
         const key = keys[i];
         const value = attributes[key];
-        result.push((<div className={"col-" + value.size}>{value.name}</div>));
+        result.push((<div key={"transaction-table-header-" + key} className={"col-" + value.size}>{value.name}</div>));
     }
     return(<div className="row">{result}</div>);
 }
